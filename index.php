577b93522438a6332371dfe4c590c98dad53ea55
Sinan Karateke <s.karateke@gmx.de>
2017-03-29 07:22:37 +0000

    PDO verschönert

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php
-/*
+<!--
 Die PHP Data Objects-Erweiterung (PDO) stellt eine leichte, 
 konsistente Schnittstelle bereit, um mit PHP auf Datenbanken zuzugreifen. 
 Jeder Datenbanktreiber, der die PDO-Schnittstelle implementiert, 
@@ -7,26 +6,39 @@ kann spezifische Features als reguläre Funktionen der Erweiterung bereitstellen
 Beachten Sie, dass Sie keine Funktionen der Datenbank mit PDO allein benutzen können. 
 Sie müssen einen datenbankspezifischen PDO-Treiber benutzen, 
 um auf eine Datenbank zuzugreifen. 
-*/
+-->
+<html>
+<head>
+</head>
+
+<?php
+// PDO Verbindungsdaten
 $dsn = "mysql:host=localhost;dbname=c9";
 $user = "markschuster";
 $pass = "";
 
+// Verbindung testen
 try {
     $db = new PDO($dsn, $user, $pass);
+    // Wenn erfolgreich, gebe aus und spiele ab
     echo "Verbindung erfolgreich";
-} catch(PDOException $e) {
+?> 
+    <!-- Erfolgreich Song-->
+    <audio autoplay preload="metadata" style=" width:300px;">
+	<source src="/Cockatiel-sings.mp3" type="audio/mpeg">
+	Your browser does not support the audio element.
+    </audio><br />
+    <!-- Erfolgreich Image-->
+    <img src="/giphy.gif" width="200px"></img>
+    
+<?php    
+// Wenn fehlerhaft, gebe aus
+} catch(PDOException $e){
     echo "Fehler: " . $e->getMessage();
 }
-
-
 ?>
-<html>
-<head>
-    
-</head>
-<body>
 
+<body>
 
 
 </body>
